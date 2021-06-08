32c781dbda67816dd07f81513c2e4ca536ffd3f0
Sandklef Henrik <hesa@sandklef.com>
2021-06-09 00:09:07 +0200

    first rev, text formatter

---
+++ scancode_manifestor/format_text.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# /usr/bin/python3
+
+###################################################################
+#
+# Scancode report -> manifest creator
+#
+# SPDX-FileCopyrightText: 2021 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+###################################################################
+
+import json
+
+class TextFormatter:
+
+    def __init__(self, utils):
+        self.utils = utils
+
+    def format(self, report):
+        res = "text report"
+
+
+
+        return res
+
+    
