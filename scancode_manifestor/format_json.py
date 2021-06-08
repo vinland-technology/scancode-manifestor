ebc6b64792f28491be37c7c0ede1f4997bd98fdf
Sandklef Henrik <hesa@sandklef.com>
2021-06-09 00:08:56 +0200

    first rev, json formatter

---
+++ scancode_manifestor/format_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# /usr/bin/python3
+
+###################################################################
+#
+# Scancode report -> manifest creator
+#
+# SPDX-FileCopyrightText: 2021 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+###################################################################
+
+import json
+
+class JSONFormatter:
+
+    def __init__(self, utils):
+        self.utils = utils
+
+    def format(self, report):
+        return str(json.dumps(report))
+
+    
