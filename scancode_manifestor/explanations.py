32d0d1fbbe5df315a7c1617fffd38106c2a9a479
Sandklef Henrik <hesa@sandklef.com>
2021-06-10 23:20:42 +0200

    scancode, licenses, excluded, included

--- scancode_manifestor/explanations.py
+++ scancode_manifestor/explanations.py
@@ -12,17 +12,59 @@
 #
 ###################################################################
 
-EXCLUDED_FILES_EXPLANATION="This sections lists the files that have been excluded and the filters. Excluding files is used to mark files that are not distributed to end user and thus not needed to consider while verifying compliance. The files have been excluded using either 'file filter' or 'license filter'. Each file is listed with the type of filter and the matching string causing the exclusion."
+## About scancode
 
-EXCLUDED_FILES_FILTER_EXPLANATION="This sections lists the file exclusion filters. For each filter a list files excluded by this filter is displayed "
+ABOUT_SCANCODE_REPORT="About the Scancode report"
 
-EXCLUDED_FILES_FILES_EXPLANATION="This sections lists the files excluded by filters. For each file the file type and the filter causing the exclusions is displayed"
+## Licenses section
 
-LICENSE_EXPLANATION="This section lists the licenses of the included and excluded files"
+LICENSES_EXPLANATION="This section lists the licenses of the included and excluded files"
 
-LICENSE_ALL_EXPLANATION="This section lists the licenses of the all the files, regardless of whether they are included or excluded"
+LICENSES_HEADER = "Licenses"
 
-LICENSE_INCLDUED_EXPLANATION="This section lists all the licenses of the included files. Each license has a list of files licensed under that licenes"
+LICENSES_INCLUDED_FILES_HEADER = "Licenses for included files"
 
-LICENSE_EXCLDUED_EXPLANATION="This section lists all the licenses of the excluded files. Each license has a list of files licensed under that licenes"
+LICENSES_EXCLUDED_FILES_HEADER = "Licenses for excluded files"
 
+LICENSES_ALL_EXPLANATION="This section lists the licenses of the all the files, regardless of whether they are included or excluded"
+
+LICENSES_INCLDUED_EXPLANATION="This section lists all the licenses of the included files. Each license has a list of files licensed under that licenes. Files are linked to the actual files so you can easily check the source code yourself."
+
+LICENSES_EXCLDUED_EXPLANATION="This section lists all the licenses of the excluded files. Each license has a list of files licensed under that licenes. Files are linked to the actual files so you can easily check the source code yourself"
+
+
+## All files section
+
+LICENSES_ALL_FILES_EXPLANATION="All licenses with files (under that license)."
+
+LICENSES_ALL_FILES_HEADER="Licenses and files"
+
+
+
+## Excluded files section
+
+EXCLUDED_FILES_EXPLANATION="This sections lists the files that have been excluded and the filters. Excluding files is used to mark files that are not distributed to end user and thus not needed to consider while verifying compliance. The files have been excluded using either 'file filter' or 'license filter'."
+
+EXCLUDED_FILES_HEADER="Excluded files (by filters)"
+
+EXCLUDED_FILES_FILTERS_HEADER="Exclusion filters"
+
+EXCLUDED_FILES_FILES_HEADER="Excluded files"
+
+EXCLUDED_FILES_FILTERS_EXPLANATION="This sections lists the file exclusion filters. For each filter a list files excluded by this filter is displayed. Files are linked to the actual files so you can easily check the source code yourself."
+
+EXCLUDED_FILES_FILES_EXPLANATION="This sections lists the files excluded by filters. For each file the file type and the filter causing the exclusions is displayed. Files are linked to the actual files so you can easily check the source code yourself. The filter is linked to the filter list in section \"" + EXCLUDED_FILES_FILTERS_HEADER + "\"."
+
+## Included files section
+
+INCLUDED_FILES_EXPLANATION="This sections lists the files that have been included, i.e distributed."
+
+INCLUDED_FILES_HEADER="Included files by filters"
+
+INCLUDED_FILES_FILTERS_HEADER="Inclusion filters"
+
+INCLUDED_FILES_FILES_HEADER="Included files"
+
+INCLUDED_FILES_FILTERS_EXPLANATION="This sections lists the file inclusion filters. For each filter a list files included by this filter is displayed. Files are linked to the actual files so you can easily check the source code yourself."
+
+INCLUDED_FILES_FILES_EXPLANATION="This sections lists the files included by filters. For each file the file type and the filter causing the inclusions is displayed. Files are linked to the actual files so you can easily check the source code yourself. The filter is linked to the filter list in section \"" + INCLUDED_FILES_FILTERS_HEADER + "\"."
