953d1d5c184719076da7cfbd1905a73a21236f85
Sandklef Henrik <hesa@sandklef.com>
2021-06-30 23:13:16 +0200

    collapsible, curations, misc input from users

--- scancode_manifestor/format_markdown.py
+++ scancode_manifestor/format_markdown.py
@@ -14,10 +14,10 @@ import datetime
 import getpass
 import json
 import os
+import sys
 
 from scancode_manifestor.manifestor_utils import FilterAttribute
 import scancode_manifestor.explanations
-
 
 
 class MarkdownFormatter:
@@ -28,6 +28,20 @@ class MarkdownFormatter:
         self.incl_lic_file = None
         self.excl_lic_file = None
 
+    def _collapse_begin(self, open=False, message="Click to expand"):
+        open_string = ""
+        
+        if open:
+            open_string = " open"
+            
+        return "<details" + open_string +  ">\n<summary>" + message + "</summary>\n\n"
+        
+    def _collapse_begin_open(self, message="Click to collapse"):
+        return self._collapse_begin(True, message)
+        
+    def _collapse_end(self):
+        return "</details>\n"
+        
     def _file_name_url(self, fn):
         return "[" + fn + "](" + fn + ")"
 
@@ -71,6 +85,7 @@ class MarkdownFormatter:
         if self.args['add_explanations']:
             res += "*" + scancode_manifestor.explanations.EXCLUDED_FILES_FILTERS_EXPLANATION + "*"
             res += "\n\n"
+        res += self._collapse_begin()        
         for re_list in report["meta"]["arguments"]["excluded_regexps"]:
             for re in re_list:
                 res += str(" * <a name=\"" + re + "\"></a>" + re + "\n\n" )
@@ -80,12 +95,13 @@ class MarkdownFormatter:
                         manifestor_map = f['scancode_manifestor']
                         #print("f: " + str(f['path']) + " " + str(f['type']) + " " + str(manifestor_map['filter_type']) + " " + str(manifestor_map['filter_expr']) + "== " + str(re) + "<br>")
                         if f['type'] == 'file':
-                            #res+=" file"
+                            #res += " file"
                             if manifestor_map['filter_type'] == FilterAttribute.PATH:
                                 #res+=" filter"
                                 if re == manifestor_map['filter_expr']:
                                     res += "    * " + self._file_url(f) +  "\n\n"
                 res += "\n\n"
+        res += self._collapse_end()        
                 
         res += "\n\n"
         res += "<a name=\"" + scancode_manifestor.explanations.EXCLUDED_FILES_FILES_HEADER + "\"></a>\n\n"
@@ -94,9 +110,12 @@ class MarkdownFormatter:
         if self.args['add_explanations']:
             res += "*" + scancode_manifestor.explanations.EXCLUDED_FILES_FILES_EXPLANATION + "*"
             res += "\n\n"
+        res += self._collapse_begin()        
         for f in files:
             if f['type'] == 'file':
                 res += self._excluded_file(f) + "\n\n"
+        res += self._collapse_end()        
+
         return res
 
     def _included_file(self, f):
@@ -107,6 +126,7 @@ class MarkdownFormatter:
         res += "\n\n"
         res += "    * ***original license***: " + str(manifestor_map['license_key'])
         res += "\n\n"
+
         if 'curated_license' in manifestor_map:
             c_license = manifestor_map['curated_license']
             res += "    * ***curated license***: " + c_license 
@@ -132,6 +152,77 @@ class MarkdownFormatter:
             pass
         return res 
 
+    def _curated_files(self, report):
+        files = report['files']['included']
+        res = "<a name=\"" + scancode_manifestor.explanations.CURATED_FILES_HEADER + "\"></a>\n\n"
+        res += "# 7 " + scancode_manifestor.explanations.CURATED_FILES_HEADER
+        res += "\n\n"
+        if self.args['add_explanations']:
+            res += "*" + scancode_manifestor.explanations.CURATED_FILES_EXPLANATION + "*"
+            res += "\n\n"
+        res += "<a name=\"" + scancode_manifestor.explanations.CURATED_FILES_FILE_HEADER + "\"></a>\n\n"
+        res += "### 7.1 " + scancode_manifestor.explanations.CURATED_FILES_FILE_HEADER
+        res += "\n\n"
+        if self.args['add_explanations']:
+            res += "*" + scancode_manifestor.explanations.CURATED_FILES_FILE_EXPLANATION + "*"
+            res += "\n\n"
+        
+        if self.args['add_explanations']:
+            res += "*" + scancode_manifestor.explanations.CURATED_FILES_FILE_EXPLANATION + "*"
+            res += "\n\n"
+        res += "\n\n"
+        res += self._collapse_begin()
+
+        for curation in report['meta']['arguments']['file_curations']:
+            curation_expr = curation[:-1]
+            curation_license = curation[-1]
+            res += " * " + str(curation_expr) + "\n\n"
+            for f in files:
+                if f['type'] == 'file':
+                    assert 'scancode_manifestor' in f
+                    manifestor_map = f['scancode_manifestor']
+                    if 'curated_license' in manifestor_map:
+                        if manifestor_map['curated_license'] == curation_license:
+                            c_license = manifestor_map['curated_license']
+                            o_license = manifestor_map['license_key']
+                            c_type = manifestor_map['curation_type']
+                            c_expr = manifestor_map['curation_expr']
+                            if c_type == 'file':
+                                res += "    * " + self._file_name_url(f['path']) + "\n\n"
+                                res += "        * ***original license:*** " + o_license + "\n\n" 
+                                res += "        * ***curated license:*** \"" +  curation_license + "\n\n" 
+                    
+        res += self._collapse_end()        
+
+        res += "<a name=\"" + scancode_manifestor.explanations.CURATED_FILES_MISSING_HEADER + "\"></a>\n\n"
+        res += "### 7.2 " + scancode_manifestor.explanations.CURATED_FILES_MISSING_HEADER
+        res += "\n\n"
+        if self.args['add_explanations']:
+            res += "*" + scancode_manifestor.explanations.CURATED_FILES_MISSING_EXPLANATION + "*"
+            res += "\n\n"
+        res += self._collapse_begin()        
+        curation_license = report['meta']['arguments']['missing_license_curation']
+        for f in files:
+            if f['type'] == 'file':
+                assert 'scancode_manifestor' in f
+                manifestor_map = f['scancode_manifestor']
+                if 'curated_license' in manifestor_map:
+                    license_key = manifestor_map['license_key']
+                    if license_key == "None" or license_key == None:
+                        #print("f: " + f['path'] + " ==> \"" + str(license_key) + "\"", file=sys.stderr)
+                        c_license = manifestor_map['curated_license']
+                        o_license = manifestor_map['license_key']
+                        c_type = manifestor_map['curation_type']
+                        c_expr = manifestor_map['curation_expr']
+                        #print("   \"" + c_type + "\"", file=sys.stderr)
+                        #print("   \"" + c_expr + "\"", file=sys.stderr)
+                        if c_type == 'license' and c_expr == "[]":
+                            res += " * " + self._file_name_url(f['path']) + "\n\n"
+        res += self._collapse_end()        
+
+        return res
+        
+
     def _included_files(self, report):
         files = report['files']['included']
 
@@ -144,9 +235,12 @@ class MarkdownFormatter:
         res += "<a name=\"" + scancode_manifestor.explanations.INCLUDED_FILES_FILES_HEADER + "\"></a>\n\n"
         res += "### 6.1 " + scancode_manifestor.explanations.INCLUDED_FILES_FILES_HEADER
         res += "\n\n"
+        res += self._collapse_begin()        
         for f in files:
             if f['type'] == 'file':
                 res += self._included_file(f)+ "\n\n"
+        res += self._collapse_end()        
+
         return res
 
     def _add_to_lic_file_map(self, f):
@@ -212,12 +306,14 @@ class MarkdownFormatter:
         if self.args['add_explanations']:
             res += "*" + scancode_manifestor.explanations.LICENSES_ALL_FILES_EXPLANATION + "*"
             res += "\n\n"
+        res += self._collapse_begin()
         for k,v in combined_license_files.items():
             res += " * " + str(k)
             res += "\n\n"
             for f in v:
                 res += "    * " + self._file_name_url(f)
                 res += "\n\n"
+        res += self._collapse_end()
             
         # included files
         res += "<a name=\"" + scancode_manifestor.explanations.LICENSES_INCLUDED_FILES_HEADER + "\"></a>\n\n"
@@ -226,12 +322,14 @@ class MarkdownFormatter:
         if self.args['add_explanations']:
             res += "*" + scancode_manifestor.explanations.LICENSES_INCLDUED_EXPLANATION + "*"
             res += "\n\n"
+        res += self._collapse_begin_open()
         for k,v in self.incl_lic_file.items():
             res += " * " + str(k)
             res += "\n\n"
             for f in v:
                 res += "    * " + self._file_url(f)
                 res += "\n\n"
+        res += self._collapse_end()
         
         # excluded files
         res += "<a name=\"" + scancode_manifestor.explanations.LICENSES_EXCLUDED_FILES_HEADER + "\"></a>\n\n"
@@ -240,14 +338,16 @@ class MarkdownFormatter:
         if self.args['add_explanations']:
             res += "*" + scancode_manifestor.explanations.LICENSES_EXCLDUED_EXPLANATION + "*"
             res += "\n\n"
+        res += self._collapse_begin()
         for k,v in self.excl_lic_file.items():
             res += " * " + k
             res += "\n\n"
             for f in v:
                 res += "    * " + self._file_url(f)
                 res += "\n\n"
+        res += self._collapse_end()
         return res
-
+    
     def _conclusion_summary(self, report):
         if self.args['no_conclusion']:
             return ""
@@ -262,13 +362,15 @@ class MarkdownFormatter:
         res += "<a name=\"" + scancode_manifestor.explanations.CONCLUSION_COPYRIGHTS_HEADER + "\"></a>\n\n"
         res += "## 3.1 "  + scancode_manifestor.explanations.CONCLUSION_COPYRIGHTS_HEADER
         res += "\n\n"
+        res += self._collapse_begin(False, "Click to expand list of copyright holders")
         if self.args['add_explanations']:
             res += "*" + scancode_manifestor.explanations.CONCLUSION_COPYRIGHTS_EXPLANATION + "*"
             res += "\n\n"
         for c in report['conclusion']['copyright']:
             res += " * " + str(c)
             res += "\n\n"
-        res += "*Note: the copyrigh holders listed  here may not be complete*"
+        res += self._collapse_end()
+        res += "*Note: the copyright holders listed here may not be complete*"
         res += "\n\n"
 
         res += "<a name=\"" + scancode_manifestor.explanations.CONCLUSION_LICENSES_HEADER + "\"></a>\n\n"
@@ -311,6 +413,9 @@ class MarkdownFormatter:
         res += " 1 Scancode manifestor report"
         res += "\n\n"
         res += " 2 " + self._header_link(scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT)
+        res += " 2.1 " + self._header_link(scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_SCANCODE)
+        res += " 2.2 " + self._header_link(scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_META)
+        res += " 2.3 " + self._header_link(scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_SETTINGS)
         res += " 3 " +   self._header_link(scancode_manifestor.explanations.CONCLUSION_HEADER)
         res += " 3.1 " +   self._header_link(scancode_manifestor.explanations.CONCLUSION_COPYRIGHTS_HEADER)
         res += " 3.2 " +   self._header_link(scancode_manifestor.explanations.CONCLUSION_LICENSES_HEADER)
@@ -324,8 +429,15 @@ class MarkdownFormatter:
         res += " 5.2 " +   self._header_link(scancode_manifestor.explanations.EXCLUDED_FILES_FILES_HEADER)
         res += " 6 " +   self._header_link(scancode_manifestor.explanations.INCLUDED_FILES_HEADER)
         res += " 6.1 " +   self._header_link(scancode_manifestor.explanations.INCLUDED_FILES_FILES_HEADER)
+        res += " 7 " +   self._header_link(scancode_manifestor.explanations.CURATED_FILES_HEADER)
+        res += " 7.1 " +   self._header_link(scancode_manifestor.explanations.CURATED_FILES_FILE_HEADER)
+        res += " 7.1 " +   self._header_link(scancode_manifestor.explanations.CURATED_FILES_MISSING_HEADER)
         res += "\n\n"
+        res += "<a name=\"" + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT + "\"></a>\n\n"
         res += "# 2 " + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT
+        res += "\n\n"
+        res += "<a name=\"" + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_SCANCODE + "\"></a>\n\n"
+        res += "## 2.1 " + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_SCANCODE
         res += "\n\n"
         scan_str = None
         if 'meta' in report:
@@ -342,7 +454,8 @@ class MarkdownFormatter:
 
         res += scan_str
         
-        res += "## Meta information report"
+        res += "<a name=\"" + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_META + "\"></a>\n\n"
+        res += "## 2.2 " + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_META
         res += "\n\n"
         res += " * ***date***: " + str(datetime.datetime.now())
         res += "\n\n"
@@ -354,6 +467,15 @@ class MarkdownFormatter:
         res += "\n\n"
         res += " * ***node***: " + uname.nodename
         res += "\n\n"
+        res += "\n\n"
+
+        res += "<a name=\"" + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_SETTINGS + "\"></a>\n\n"
+        res += "## 2.3 " + scancode_manifestor.explanations.ABOUT_SCANCODE_REPORT_SETTINGS
+        res += "\n\n"
+        res += self._collapse_begin()
+        res += "\n" + str(json.dumps(report['meta'], indent=4)).replace("\n","\n\n") + "\n"
+        res += self._collapse_end()        
+        
         res += "\n\n"
 
         res += self._conclusion_summary(report)
@@ -369,6 +491,10 @@ class MarkdownFormatter:
         res += "\n\n"
 
         res += self._included_files(report)
+
+        res += "\n\n"
+
+        res += self._curated_files(report)
 
         res += "\n\n"
 
