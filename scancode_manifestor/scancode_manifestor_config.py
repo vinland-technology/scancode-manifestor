e704b8739d62fda9345a3615b1f4c1664ef25255
Sandklef Henrik <hesa@sandklef.com>
2021-12-27 16:41:56 +0100

    new file - version info

---
+++ scancode_manifestor/scancode_manifestor_config.py
@@ -0,0 +1,11 @@
+###################################################################
+#
+# Scancode report -> manifest creator
+#
+# SPDX-FileCopyrightText: 2021 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+###################################################################
+
+scancode_manifestor_version = "0.1"
