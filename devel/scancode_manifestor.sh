0dc84bdaaa4cc5f8d1f95829f95cec212580966a
Sandklef Henrik <hesa@sandklef.com>
2022-01-21 11:31:13 +0100

    add setup help option

--- devel/scancode_manifestor.sh
+++ devel/scancode_manifestor.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})/.."
-
+if [ "$1" = "--setup" ]
+then
+    echo export PYTHONPATH=${SCRIPT_PATH}
+    echo alias scancode_manifestor=\"${SCRIPT_PATH}/scancode_manifestor/__main__.py\"
+    echo alias scancode-manifestor=\"${SCRIPT_PATH}/scancode_manifestor/__main__.py\"
+    exit 0
+fi
 PYTHONPATH=${SCRIPT_PATH} ${SCRIPT_PATH}/scancode_manifestor/__main__.py $*
+
 #ls -al ${SCRIPT_PATH} ${SCRIPT_PATH}/scancode_manifestor/__main__.py
